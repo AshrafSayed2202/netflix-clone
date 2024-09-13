b28c4b4f7a23cbf61c4cb4c42c12a0ba7fb094d1
ashraf9932 <ashraf.neizk@gmail.com>
2024-09-13 23:35:40 +0300

    fix images

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,6 @@
 const Auth = () => {
     return (
-        <div className="relative h-full w-full bg-[url('/images/hero.jpg')]">
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-cover bg-center">
 
         </div>
     )
